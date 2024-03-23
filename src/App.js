fe6ef7d2d5c4cf4b8c89e2ae4c54340ba300a59c
rei-takahashi <xxx@users.noreply.github.com>
2024-03-23 12:30:12 +0900

    ルーティング設定とstyle.cssの解消

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/playerList/RegisterPlayer";
+import PlayerList from "./pages/playerList/PlayerList";
+import RegisterOpponent from "./pages/opponent/RegisterOpponent";
+import OpponentList from "./pages/opponent/OpponentList";
 import "./App.css"
 import Match from "./pages/match/Match";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Menu from "./pages/menu/Menu";
+import MatchList from "./pages/match/MatchList";
+import PlayerDetails from "./pages/playerList/PlayerDetails";
+import OpponentDetails from "./pages/opponent/OpponentDetails";
 
 function App() {
   return (
-  <>
-      {/* <Login /> */}
-      <Home />
-      {/* <Match /> */}
-  </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/gamelist" element={<MatchList />}/>
+        <Route path="/newgame" element={<Match />}/>
+        <Route path="/playerlist" element={<PlayerList />}/>
+        <Route path="/playerlist/sawamura" element={<PlayerDetails />}/>
+        <Route path="/playerRegister" element={<Register />}/>
+        <Route path="/opponentRegister" element={<RegisterOpponent/>}/>
+        <Route path="/opponentlist" element={<OpponentList/>}/>
+        <Route path="/opponentlist/karasuno" element={<OpponentDetails />}/>
+      </Routes>
+    </Router>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import "./Home.css"
 import Topbar from '../../components/topbar/Topbar'
 import Sidebar from "../../components/sidebar/Sidebar"
-import Menu from '../menu/Menu.jsx'
+// import Menu from '../menu/Menu.jsx'
 import PlayerList from '../playerList/PlayerList.jsx'
 import RegisterOpponent from '../opponent/RegisterOpponent.jsx'
 import OpponentList from '../opponent/OpponentList.jsx'
@@ -12,6 +12,7 @@ import Register from '../playerList/RegisterPlayer.jsx'
 import OpponentDetails from '../opponent/OpponentDetails.jsx'
 import PlayerDetails from '../playerList/PlayerDetails.jsx'
 import MatchList from '../match/MatchList.jsx'
+// import { Link } from 'react-router-dom'
 
 export default function Home() {
 
@@ -19,16 +20,15 @@ export default function Home() {
 
   return (
     <>  
-        {/* <Topbar opponent={opponent} key={opponent.id} /> */}
         <div className="homeContainer">
             {/* <Menu /> */}
-            {/* <PlayerList /> */}
-            {/* <PlayerDetails /> */}
-            {/* <Register /> */}
-            {/* <RegisterOpponent/> */}
-            {/* <OpponentList/> */}
-            {/* <OpponentDetails /> */}
-            <MatchList />
+            {/* <PlayerList />
+            <PlayerDetails />
+            <Register />
+            <RegisterOpponent/>
+            <OpponentList/>
+            <OpponentDetails />
+            <MatchList /> */}
         </div>
     </>
   )
